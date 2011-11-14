933cc932ba14352de8c8ff1e1fff326ec614c9d1
James Gaston <jamesgaston@yahoo.com>
2011-11-14 13:26:01 -0800

    Move db queries to models, replace readme

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,16 @@
 class ItemsController < ApplicationController
 
   def new
-   @categories = Category.order("name ASC")
-   @ownerships = Ownership.order("name ASC")
+   @categories = Category.ordered 
+	@ownerships = Ownership.ordered
 	@item = Item.new
   user = current_user
 	@item.user_id = user.id
   end
 
   def edit
-   @categories = Category.order("name ASC")
-   @ownerships = Ownership.order("name ASC")
+   @categories = Category.ordered 
+	@ownerships = Ownership.ordered
 	@item = Item.find_by_id( params[:id] )
   end
 
--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,2 +1,6 @@
 class Ownership < ActiveRecord::Base
+has_many :items
+
+scope :ordered, order("name")
+
 end
